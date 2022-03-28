767e6685207ac5f2534769773e3f8041bcbe2485
Lucas Dührsen <lucas.duehrsen@tum.de>
2022-03-28 21:31:37 +0200

    Remove unnecessary type in event handler

--- src/renderer/components/manifest-creation/step-three/StepThree.tsx
+++ src/renderer/components/manifest-creation/step-three/StepThree.tsx
@@ -37,11 +37,7 @@ function StepThree(props: any) {
                   label="Abort On Exception"
                   value={settings.abortOnException ? 'true' : 'false'}
                   helperText="By default, the BLF will not abort when encountering an exception. It will instead try to carry on the rest of its work as best as it can. Let's say you are analyzing 1000 blocks of a blockchain. Just because there was an error extracting information from one block, the BLF might still be able to extract information from the rest of them. If you instead want it to directly abort the mission with an error code, you can use"
-                  onChange={(
-                    event:
-                      | React.ChangeEvent<HTMLSelectElement>
-                      | React.ChangeEvent<HTMLInputElement>
-                  ) => {
+                  onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
                     setSettings(event, true);
                   }}
                 />
